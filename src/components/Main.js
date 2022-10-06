9f4fffc2d3c58c112aa85c496210a315b439f276
Kristopher <kristopher_matic@hotmail.com>
2022-10-05 19:44:41 -0700

    finish migrating from component state to redux state

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,44 +2,9 @@ import React from 'react';
 import Nav from './main/Nav';
 import Todos from './main/Todos';
 import styled from 'styled-components';
-import { useState, useEffect } from 'react';
 import { ProjectModal, TodoModal } from './modal/Modal';
-import { v4 as uuidv4 } from 'uuid';
 
 const Main = () => {
-    const fields = {
-        project: {
-            id: uuidv4(),
-            projectName: '',
-            todos: [],
-        },
-        todo: {
-            id: uuidv4(),
-            title: '',
-            description: '',
-            dueDate: '',
-            priority: 'High',
-        },
-        projects: [{ id: uuidv4(), projectName: 'Inbox', todos: [] }],
-    };
-
-    const [data, setData] = useState(fields);
-
-    const closeEditModal = () => {
-        setData((prevState) => ({
-            ...prevState,
-            todo: {
-                id: uuidv4(),
-                title: '',
-                description: '',
-                dueDate: '',
-                priority: 'High',
-            },
-        }));
-
-        // setShowTodoModal(false);
-    };
-
     return (
         <>
             <MainWrapper>
